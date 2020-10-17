e5cd549970eabbfa6eb630a7652f775f75855f1a
Dwi Yulianto <98dwiyulianto@gmail.com>
2020-10-17 09:01:22 +0700

    tugas 15 JS
    tugas untuk Niomic

---
+++ tugas15.js
@@ -0,0 +1,7 @@
+let AB = 8;
+let BC = 6;
+
+console.log('Panjang nilai AB : ' + AB);
+console.log('Panjang nilai BC : ' + BC);
+
+console.log('panjang sisi AC pada segitiga siku-siku tersebut adalah : ' + Math.sqrt(Math.pow(AB, 2) + Math.pow(BC, 2)));
